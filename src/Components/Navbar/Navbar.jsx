016b0a79e8415659670856e893be6b563b848e2b
Anuththara Imanshi <anuththaraimanshi842@gmail.com>
2025-08-10 12:39:09 +0100

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,6 +11,16 @@ const Navbar = () => {
         <p>Shopper</p>
 
     </div>
+    <ul className="nav-menu">
+      <li>Shop</li>
+      <li>Menu</li>
+      <li>Women</li>
+      <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+    </div>
     </div>
   )
 }
